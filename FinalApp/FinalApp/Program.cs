483f5a225c1b3625a5054c32d649fededa637e78
Micah Bissell <mbissell@students.ccu.edu>
2024-05-07 08:42:26 -0600

    Program

---
+++ FinalApp/FinalApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+public static class DaysCalc
+{
+    static void Main(string[] calc)
+    {
+        Console.WriteLine("What is your birth date? (yyyy/mm/dd)");
+        if (!DateTime.TryParse(Console.ReadLine(), out DateTime birthday))
+        {
+            Console.WriteLine("Invalid date");
+            return;
+        }
+
+
+        int daysOld = (DateTime.Today - birthday).Days;
+        Console.WriteLine($"You are {daysOld} days old.");
+    }
+}
